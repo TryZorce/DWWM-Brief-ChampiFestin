37dea69b54cf93955ad338207ddbf50b04c82694
Arno <arno.pimenta@gmail.com>
2023-07-19 16:50:33 +0200

    Finish Hard Carousel

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'primeicons/primeicons.css'
+import '/node_modules/primeflex/primeflex.css'
+
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";                                       
+        
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
