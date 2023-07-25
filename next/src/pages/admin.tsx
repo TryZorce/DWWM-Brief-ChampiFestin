81df83a9ee1b1c48401d7c37786e5396133e667c
TryZorce <christopher.moron@gmail.com>
2023-07-25 14:26:21 +0200

    Add Cart page and component

--- next/src/pages/admin.tsx
+++ next/src/pages/admin.tsx
@@ -4,7 +4,6 @@ const Admin = () => {
 
     return (
         <div>
-            {/* Aquí colocas tu contenido del componente Admin */}
             <br />
             <Link href="http://localhost:8000/admin"><img src="https://media.tenor.com/6p7YYIxqMo4AAAAd/food-dance.gif" alt="" /></Link>
         </div>
